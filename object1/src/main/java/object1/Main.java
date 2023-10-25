704cb0013dca0775f9c469e61777041d665fd64f
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-25 15:43:49 -0400

    final

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,10 +5,15 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
+        Person person2 = new Person("Bob", 32, 186);
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+        logger.info(person2.getHeight() +"");
+        System.out.println(person2.getHeight());
+    }
 
-    Person person = new Person();
+
+
 }
 
 
--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -4,15 +4,21 @@ class Person {
     String name;
     int age = 0;
 
-    int height;
+    double height;
 
     // Constructor
-    public Person(String name, int age, int height) {
+    public Person(String name, int age, double height) {
         this.name = name;
         this.age = age;
         // height in centimeters
         this.height = height;
+
+
+        }
+    public double getHeight() {
+            return height;
     } // Declare and initialize an object
-    Person person1 = new Person("John", 30, 165);
+
+
 }
 
